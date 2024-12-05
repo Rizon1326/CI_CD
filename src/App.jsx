e5e26f177c7e12f21e3ccfbbc37e7c3c0e27273e
Rizon1326 <bsse1326@iit.du.ac.bd>
2024-12-05 19:41:42 +0600

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <h1>Welcome to My Vite React App!</h1>
       <p>This is a simple app deployed on Vercel with CI/CD using GitHub Actions.</p>
-      <p>New changegit</p>
+      <p>New change git</p>
+      <p>Another change git</p>
     </div>
   );
 }
