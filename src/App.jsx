d903dabe30be336dc814414fa2c58929fa4c5d24
Rizon1326 <bsse1326@iit.du.ac.bd>
2024-12-05 18:55:44 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My Vite React App!</h1>
+      <p>This is a simple app deployed on Vercel with CI/CD using GitHub Actions.</p>
+    </div>
+  );
+}
+
+export default App;
